7afc102c2d243cb657eacdb7f8a468e7c4307876
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-15 17:31:00 -0500

    built form for the friends list, trying to figure out how to post the info

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+
 
 const FriendsList = () => {
+    const [friends, setFriends] = useState({id: Date.now()});
+
+    const handleChanges = e => {
+        setFriends({
+            ...friends,
+            [e.target.name]: e.target.value
+            }
+        )
+        console.log(friends)
+    }
+    useEffect(() => {
+        axios
+        .get('http://localhost:5000/api/friends')
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log('data not coming', err)
+        })
+        }
+    )
+
+    const submit = e => {
+        axios
+        .post('http://localhost:5000/api/friends', friends)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log('could not add friend', err)
+        })
+    }
 
     return(
-        <h1>Friends List</h1>
+        <>
+            <h1>Friends List</h1>
+            <form onSubmit={submit}>
+                <input
+                    placeholder='Name'
+                    type='text'
+                    name='name'
+                    value={friends.name}
+                    onChange={handleChanges}
+                />
+                <input
+                    placeholder='age'
+                    type='text'
+                    name='age'
+                    value={friends.age}
+                    onChange={handleChanges}
+                />
+                <input
+                    placeholder='email'
+                    type='text'
+                    name='email'
+                    value={friends.email}
+                    onChange={handleChanges}
+                />
+                <button>Add Friend </button>
+            </form>
+
+        </>
     )
 };
 
